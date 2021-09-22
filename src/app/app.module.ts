77d7dcac53f073e14626bf962df52ed582580d74
Tomasz Gądek <tomasz@eduj.pl>
2021-09-22 12:43:34 +0200

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ShoppingCartComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
